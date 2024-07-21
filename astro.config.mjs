990875e616aaa9e540a9eba0e4ea4c1ca88bf24a
sekalengrengginang <49896401+sekalengrengginang@users.noreply.github.com>
2024-07-21 21:21:35 +0700

    feat:fix astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,20 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import partytown from '@astrojs/partytown';
+import partytown from '@astrojs/partytown'
+
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.kizunacatering.my.id/',
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  integrations: [mdx(), sitemap(), partytown({
-    config: {
-      forward: ["dataLayer.push"]
-    }
-  }), tailwind(),, react()]
+  integrations: [
+    mdx(), 
+		sitemap(),
+		partytown({
+			config: {
+			  forward: ["dataLayer.push"],
+			},
+		}),
+    tailwind(),]
 });
