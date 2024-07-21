4bcc8e1fef0cec2ebfa82b3d071e750f479538ed
sekalengrengginang <49896401+sekalengrengginang@users.noreply.github.com>
2024-07-21 20:46:25 +0700

    feat: refactor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,18 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import partytown from '@astrojs/partytown'
-
+import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.kizunacatering.my.id/',
-  integrations: [
-    mdx(), 
-		sitemap(),
-		partytown({
-			config: {
-			  forward: ["dataLayer.push"],
-			},
-		}),
-    tailwind(),]
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  integrations: [mdx(), sitemap(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), tailwind(),, react()]
 });
